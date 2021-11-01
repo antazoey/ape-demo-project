c22fadc426da4d35536c2a8bb55233cece4c2f04
unparalleled-js <yingthi@live.com>
2021-11-01 11:22:46 -0500

    fix: requirements

--- scripts/utils.py
+++ scripts/utils.py
@@ -3,9 +3,5 @@ from ape import project
 
 
 def deploy():
-    account = get_account("Select an account to deploy 'Fund'")
+    account = get_user_selected_account("Select an account to deploy 'Fund'")
     return account.deploy(project.Fund)
-
-
-def get_account(promt=None):
-    return get_user_selected_account(prompt_message=promt)
