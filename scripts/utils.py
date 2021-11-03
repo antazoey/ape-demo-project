0495ef1975aac31c6704aa87929f3cc014b33965
unparalleled-js <yingthi@live.com>
2021-11-03 14:49:07 -0500

    feat: use index based for test

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,20 @@
 from ape.cli import get_user_selected_account
+from ape import accounts
+from ape import networks
 from ape import project
 
 
 def deploy(gas_limit=None):
-    account = get_user_selected_account(
+    account = get_account()
+    return account.deploy(project.Fund, gas_limit=gas_limit)
+
+
+def get_account():
+    network_name = networks.active_provider.name
+    if network_name == "test":
+        breakpoint()
+        return accounts[0]
+
+    return get_user_selected_account(
         prompt_message="Select an account to deploy 'Fund'"
     )
-    return account.deploy(project.Fund, gas_limit=gas_limit)
