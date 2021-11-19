e42932ed96dc9f4c0fdd5a504216ba6f3828bdd6
unparalleled-js <yingthi@live.com>
2021-11-19 10:11:49 -0600

    feat: revert script

---
+++ scripts/revert.py
@@ -0,0 +1,17 @@
+from ape import accounts
+
+from utils import deploy
+
+
+def main():
+    """
+    Deploys a contract and forcibly cause a contract-logic revert.
+    """
+    owner = accounts.test_accounts[0]
+    funder = accounts.test_accounts[1]
+    contract = deploy(sender=owner)
+
+    contract.fund(value=1000000000, sender=funder)
+
+    # Attempting to withdraw from non-owner
+    contract.withdraw(sender=funder)
