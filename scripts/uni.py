443834b81c0eaae00dca80a0f1400ae7513e86a8
unparalleled-js <yingthi@live.com>
2021-11-30 16:38:50 -0600

    YES

--- scripts/uni.py
+++
@@ -1,7 +0,0 @@
-from ape import Contract, project
-
-UNISWAP_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-
-def main():
-    uniswap = Contract(UNISWAP_ADDRESS)
-    uniswap.swapExactETHForTokens
