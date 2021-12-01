1bc6e90cfe75bebc5711af8ff3b7b39996d4afa5
unparalleled-js <yingthi@live.com>
2021-12-01 09:34:28 -0600

    rm unneeded part

--- scripts/transfer.py
+++ scripts/transfer.py
@@ -3,4 +3,4 @@ from utils import get_account
 def main():
     sender = get_account("Which account is the sender")
     receiver = get_account("Which account is the receiver")
-    receipt = sender.transfer(receiver, "1 gwei")
+    sender.transfer(receiver, "1 gwei", required_confirmations=3)
