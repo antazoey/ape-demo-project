ae204c5d1c28e1cab082ef97c728b354f8c4fe0b
unparalleled-js <yingthi@live.com>
2021-12-06 19:27:54 -0600

    feat: script test init from addr

---
+++ scripts/addr.py
@@ -0,0 +1,15 @@
+from ape import accounts, project
+
+def main():
+    account = accounts.test_accounts[0]
+    funder = accounts.test_accounts[1]
+
+    contract = account.deploy(project.Fund)
+    
+    print("Call method from normal deployed contract...")
+    contract.fund(value=1000000000, sender=funder)
+    address = contract.address
+
+    print("Call method from contract initialized from address...")
+    contract_from_address = project.Fund.at(address)
+    contract_from_address.fund(value=1000000000, sender=funder)
