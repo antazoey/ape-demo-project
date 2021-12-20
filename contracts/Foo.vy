8d2d7a9e760262192718951797cc6a3347f56281
unparalleled-js <yingthi@live.com>
2021-12-20 17:23:07 -0600

    updates

---
+++ contracts/Foo.vy
@@ -0,0 +1,5 @@
+# @version 0.3.1
+
+@external
+def foo1() -> bool:
+    return True
---
+++ scripts/num.py
@@ -0,0 +1,9 @@
+import click
+
+from ape import networks
+
+
+def main():
+    num = networks.active_provider.get_block("latest")
+    styled_num = click.style(str(num), bold=True)
+    click.echo(f"The current block number is {styled_num}.")
