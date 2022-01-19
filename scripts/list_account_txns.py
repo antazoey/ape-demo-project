ed5d9055a1a89e676ca8e193a4aee05e450164d2
unparalleled-js <yingthi@live.com>
2022-01-19 15:32:55 -0600

    fix: fixes

--- scripts/list_account_txns.py
+++ scripts/list_account_txns.py
@@ -1,11 +1,15 @@
+from webbrowser import get
 from ape import chain
 import click
 
-from utils import get_account
+from utils import get_account, get_provider
 
 
 def main():
+    if get_provider().network.name == "development":
+        raise Exception("Must use non-development network for this script.")
+
     account = get_account()
     txns = chain.account_history[account]
     for txn in txns:
-        click.echo(txn)
+        click.echo(txn.txn_hash)
---
+++ scripts/tokens.py
@@ -0,0 +1,5 @@
+def main():
+    from ape import convert
+
+    converted_val = convert("10 LINK", int)
+    print(converted_val)
