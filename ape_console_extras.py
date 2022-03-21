67769d1224fe89b1d5fa555d831a2a4c2a5b1eee
unparalleled-js <yingthi@live.com>
2022-03-21 15:48:18 -0500

    fix: conditionally load deployments

--- ape_console_extras.py
+++ ape_console_extras.py
@@ -1,10 +1,15 @@
 def ape_init_extras(config, networks):
     ecosystem = networks.provider.network.ecosystem.name
     network = networks.provider.network.name
-    network_deployments = config.deployments[ecosystem][network]
-    deployments = [d for d in network_deployments if d["contract_type"] == "TestContract"]
-    latest_address = deployments[-1]["address"]
 
-    return {
-        "test_contract_address": latest_address,
-    }
+    extras = {}
+
+    if ecosystem in config.deployments:
+        ecosystem_deployments = config.deployments[ecosystem]
+        if network in ecosystem_deployments:
+            network_deployments = ecosystem[network]
+            deployments = [d for d in network_deployments if d["contract_type"] == "TestContract"]
+            latest_address = deployments[-1]["address"]
+            extras["test_contract_address"] = latest_address
+
+    return extras
