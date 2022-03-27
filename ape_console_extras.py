76f40817205f26d77cc1d11e1c5838b3d36cb8de
unparalleled-js <yingthi@live.com>
2022-03-27 12:20:09 -0700

    feat: feet

--- ape_console_extras.py
+++ ape_console_extras.py
@@ -1,15 +1,27 @@
-def ape_init_extras(config, networks):
+def ape_init_extras(accounts, config, networks):
     ecosystem = networks.provider.network.ecosystem.name
     network = networks.provider.network.name
 
     extras = {}
 
-    if ecosystem in config.deployments:
-        ecosystem_deployments = config.deployments[ecosystem]
-        if network in ecosystem_deployments:
-            network_deployments = ecosystem[network]
-            deployments = [d for d in network_deployments if d["contract_type"] == "TestContract"]
-            latest_address = deployments[-1]["address"]
-            extras["test_contract_address"] = latest_address
+    try:
+        if ecosystem in config.deployments:
+            ecosystem_deployments = config.deployments[ecosystem]
+            if network in ecosystem_deployments:
+                network_deployments = ecosystem[network]
+                deployments = [
+                    d for d in network_deployments if d["contract_type"] == "TestContract"
+                ]
+                latest_address = deployments[-1]["address"]
+                extras["test_contract_address"] = latest_address
+
+        # Mimic fixtures
+        extras = {
+            "owner": accounts.test_accounts[0],
+            "sender": accounts.test_accounts[1],
+            **extras,
+        }
+    except Exception:
+        pass
 
     return extras
