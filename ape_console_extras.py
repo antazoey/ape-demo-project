691dccdc26a4be8c315bcdafde93e505767fb8f6
unparalleled-js <yingthi@live.com>
2022-07-07 15:32:33 -0500

    chore: TODO

--- ape_console_extras.py
+++ ape_console_extras.py
@@ -10,40 +10,40 @@ def ape_init_extras(accounts, project, config, networks):
 
     extras = {}
 
-    # try:
-    #     if ecosystem in config.deployments:
-    #         ecosystem_deployments = config.deployments[ecosystem]
-    #         if network in ecosystem_deployments:
-    #             network_deployments = ecosystem[network]
-    #             deployments = [
-    #                 d for d in network_deployments if d["contract_type"] == "TestContract"
-    #             ]
-    #             latest_address = deployments[-1]["address"]
-    #             extras["test_contract_address"] = latest_address
+    # # try:
+    # #     if ecosystem in config.deployments:
+    # #         ecosystem_deployments = config.deployments[ecosystem]
+    # #         if network in ecosystem_deployments:
+    # #             network_deployments = ecosystem[network]
+    # #             deployments = [
+    # #                 d for d in network_deployments if d["contract_type"] == "TestContract"
+    # #             ]
+    # #             latest_address = deployments[-1]["address"]
+    # #             extras["test_contract_address"] = latest_address
 
-    #     # Mimic fixtures
-    #     owner = accounts.test_accounts[0]
-    #     logger.info(f"Deploying {project.FundMe} in ape_console_extras.py")
-    #     contract = project.FundMe.deploy(sender=owner)
-    #     extras = {
-    #         "owner": owner,
-    #         "sender": accounts.test_accounts[1],
-    #         "fund_me": contract,
-    #         "contract": contract,
-    #         **extras,
-    #     }
+    # #     # Mimic fixtures
+    # #     owner = accounts.test_accounts[0]
+    # #     logger.info(f"Deploying {project.FundMe} in ape_console_extras.py")
+    # #     contract = project.FundMe.deploy(sender=owner)
+    # #     extras = {
+    # #         "owner": owner,
+    # #         "sender": accounts.test_accounts[1],
+    # #         "fund_me": contract,
+    # #         "contract": contract,
+    # #         **extras,
+    # #     }
 
-    #     # Add remaining accounts
-    #     index = 2
-    #     for acct in accounts.test_accounts[2:]:
-    #         extras[f"acct{index}"] = acct
-    #         index += 1
+    # #     # Add remaining accounts
+    # #     index = 2
+    # #     for acct in accounts.test_accounts[2:]:
+    # #         extras[f"acct{index}"] = acct
+    # #         index += 1
 
-    # except Exception as err:
-    #     logger.error(err)
-    #     pass
+    # # except Exception as err:
+    # #     logger.error(err)
+    # #     pass
 
-    # extras["list_extras"] = lambda: click.echo(
-    #     json.dumps({k: str(v) for k, v in extras.items() if k != "list_extras"}, indent=2)
-    # )
-    return extras
+    # # extras["list_extras"] = lambda: click.echo(
+    # #     json.dumps({k: str(v) for k, v in extras.items() if k != "list_extras"}, indent=2)
+    # # )
+    # return extras
--- scripts/poll_logs.py
+++ scripts/poll_logs.py
@@ -2,6 +2,7 @@ import click
 from ape import config, networks, project
 
 
+# TODO: Parametrize and make a better example
 def main():
     event_type = "NumberChange"
     contract_name = "TestContractSol"
@@ -9,10 +10,6 @@ def main():
 
     ecosystem = networks.provider.network.ecosystem.name
     network = networks.provider.network.name.replace("-fork", "")
-    # network_deployments = config.deployments[ecosystem][network]
-    # deployments = [d for d in network_deployments if d["contract_type"] == contract_name]
-    # contract_address = deployments[0]["address"]
-
     contract_address = "0x274b028b03A250cA03644E6c578D81f019eE1323"
     contract_type = project.TestContractVy
     contract_instance = contract_type.at(contract_address)
