b3847121ee086c19cbc06d77f0acec17d4f3d6eb
unparalleled-js <yingthi@live.com>
2022-07-07 15:34:30 -0500

    chore: simp

--- scripts/poll_logs.py
+++ scripts/poll_logs.py
@@ -6,8 +6,6 @@ from ape import config, networks, project
 def main():
     event_type = project.TestContractVy.NumberChange
     click.echo(f"Polling for new '{event_type}' logs...")
-    contract_address = "0x274b028b03A250cA03644E6c578D81f019eE1323"
-    contract_type = project.TestContractVy
 
     for new_log in event_type.poll_logs():
         click.echo(new_log.newNum)
