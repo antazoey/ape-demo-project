77eca418196fd4a06d5a9ab378801535fe7c4fc2
unparalleled-js <yingthi@live.com>
2022-07-07 15:36:21 -0500

    fix: stop breaking it lol

--- scripts/poll_logs.py
+++ scripts/poll_logs.py
@@ -4,8 +4,9 @@ from ape import config, networks, project
 
 # TODO: Parametrize and make a better example
 def main():
-    event_type = project.TestContractVy.NumberChange
-    click.echo(f"Polling for new '{event_type}' logs...")
+    instance = project.TestContractVy.at("0x274b028b03A250cA03644E6c578D81f019eE1323")
+    event_type = instance.NumberChange
+    click.echo(f"Polling for new '{event_type.name}' logs...")
 
     for new_log in event_type.poll_logs():
         click.echo(new_log.newNum)
